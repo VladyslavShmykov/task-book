facbfbcd0922607a4c6e8884d9371aa3dd282059
v.shmykov <v.shmykov@e-tender.ua>
2025-02-07 17:59:39 +0200

    auth

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
   { path: 'main', loadChildren: () => import('./task/task.module').then(m => m.TaskModule) }
 ];
--- src/app/auth/auth/auth.component.ts
+++ src/app/auth/auth/auth.component.ts
@@ -3,10 +3,13 @@ import {AuthService} from "../../../shared/services/auth/auth.service";
 import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
 import {of, switchMap} from "rxjs";
 import {Router} from "@angular/router";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-auth',
-  imports: [],
+  imports: [
+    MatButton
+  ],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss'
 })
@@ -26,5 +29,13 @@ export class AuthComponent {
         this.router.navigate(['main']).then()
       }
     })
+  }
+
+  login() {
+    this.authService.signInWithGoogle().then()
+  }
+
+  logout() {
+    this.authService.logout().then()
   }
 }
